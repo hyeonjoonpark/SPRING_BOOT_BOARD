247500ddf888b4518a91400f63473397b5657541
hyeonjoonpark <pjjoon1379@gmail.com>
2024-05-27 01:34:57 +0900

    FEAT: spring boot 프로젝트 생성
    Initializer를 사용해서 프로젝트를 생성하였습니다

---
+++ board/src/test/java/com/hj/projectboard/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
