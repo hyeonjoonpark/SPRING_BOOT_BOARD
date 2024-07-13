3212c8f59f658fdaa74c9de4dd0be72cb694f66d
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-13 21:20:44 +0900

    ADD: EntityListeners

--- board/src/main/java/com/hj/projectboard/domain/Article.java
+++ board/src/main/java/com/hj/projectboard/domain/Article.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -18,6 +19,7 @@ import java.util.*;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
