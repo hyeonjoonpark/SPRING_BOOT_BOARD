a9d38049ea9542962f11144024b84239fb9ef70c
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-13 21:15:24 +0900

    FEAT: Article, ArticleComment

--- board/src/main/java/com/hj/projectboard/domain/Article.java
+++ board/src/main/java/com/hj/projectboard/domain/Article.java
@@ -1,15 +1,81 @@
 package com.hj.projectboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
+
     private String hashTag;
 
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("id")
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+
+    private Article(String title, String content, String hashTag) {
+        this.title = title;
+        this.content = content;
+        this.hashTag = hashTag;
+    }
+
+    public static Article of(String title, String content, String hashTag) {
+        return new Article(title, content, hashTag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
---
+++ board/src/main/java/com/hj/projectboard/global/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.hj.projectboard.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("hyeonjoon"); // 스프링 시큐리티 인증기능 추가 시 수정
+    }
+}
