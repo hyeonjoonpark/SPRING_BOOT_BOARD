38aa534296ff73f78c3c840423192166010b8fc8
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-20 02:46:32 +0900

    ADD: test code

---
+++ board/src/main/java/com/hj/projectboard/domain/article/controller/ArticleController.java
@@ -0,0 +1,29 @@
+package com.hj.projectboard.domain.article.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article");
+        map.addAttribute("articleComment", List.of());
+
+        return "articles/detail";
+    }
+
+}
