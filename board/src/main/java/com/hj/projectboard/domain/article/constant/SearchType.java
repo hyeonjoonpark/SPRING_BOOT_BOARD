128878a16c5aa2ce841bc5905e1169dd9ed0e3f6
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-14 21:28:32 +0900

    :))

--- board/src/main/java/com/hj/projectboard/domain/article/constant/SearchType.java
+++ board/src/main/java/com/hj/projectboard/domain/article/constant/SearchType.java
@@ -1,10 +1,8 @@
 package com.hj.projectboard.domain.article.constant;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum SearchType {
     TITLE("제목"),
     CONTENT("본문"),
@@ -13,4 +11,8 @@ public enum SearchType {
     HASHTAG("해시태그");
 
     private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
